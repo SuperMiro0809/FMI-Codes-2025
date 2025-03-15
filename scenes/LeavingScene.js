31867e3dc81f33c872cf55225c0674651912aeaa
Mihail Dimitrov <mighty.strong1235@gmail.com>
2025-03-16 01:32:12 +0200

    added a walking animation

--- scenes/LeavingScene.js
+++ scenes/LeavingScene.js
@@ -21,46 +21,46 @@ class LeavingScene extends Phaser.Scene {
     const bgScale = Math.max(width / backImage.width, height / backImage.height);
     backImage.setScale(bgScale).setScrollFactor(0);
 
-    this.avatar = this.physics.add.sprite(this.game.config.width - 250, height/1.4, 'avatar').setOrigin(0.5);
+    this.avatar = this.physics.add.sprite(this.game.config.width - 250, height / 1.4, 'avatar').setOrigin(0.5);
     this.avatar.setScale(2.2); 
 
     this.alien = this.physics.add.sprite(300, height / 1.25, 'alien').setOrigin(0.5);
-    this.alien.setScale(.3);
+    this.alien.setScale(0.3);
 
     this.character = this.physics.add.sprite(300, height / 1.2, 'character').setOrigin(0.5);
     this.character.setScale(2);
-    this.character.play('walk_left') 
-    this.startCharacterWalk();
 
     this.anims.create({
       key: 'walk',
       frames: this.anims.generateFrameNumbers('character', { start: 0, end: 3 }), 
       frameRate: 8,
-      repeat: 1
+      repeat: -1
     });
+
+    this.startCharacterWalk();
   }
 
-
-
   startCharacterWalk() {
+    this.character.play('walk');
+
     this.tweens.add({
       targets: this.character,
       x: this.sys.game.canvas.width - 270, 
       duration: 3000,
       ease: 'Linear',
       onComplete: () => {
-        this.character.destroy();
+        this.character.destroy(); 
+        
         this.tweens.add({
           targets: this.avatar,
-          y: -this.avatar.height, // Move completely out of view
+          y: -this.avatar.height,
           duration: 2000,
-          ease: 'Linear',
+          ease: 'Cubic.easeIn',
           onComplete: () => {
-            this.scene.start("MenuScene")
+            this.scene.start("MenuScene");
           }
         });
       }
     });
   }
-
 }
