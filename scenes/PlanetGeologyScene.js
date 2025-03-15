ff25c92e0213bd092adf6faee23410f00bc631f8
Maxim <hristivi8383@gmail.com>
2025-03-15 03:07:25 +0200

    Ground digging - down

--- scenes/PlanetGeologyScene.js
+++ scenes/PlanetGeologyScene.js
@@ -1,6 +1,6 @@
-const tileSize = 32;
+const tileSize = 40;
 const worldWidth = 400;
-const worldHeight = 1000;
+const worldHeight = window.innerHeight - 200;
 const playerSpeed = 100;
 
 class PlanetGeologyScene extends Phaser.Scene {
@@ -8,21 +8,54 @@ class PlanetGeologyScene extends Phaser.Scene {
     super({ key: 'PlanetGeologyScene' });
   }
 	preload() {
-    //load miner.webp as a sprite
-		this.load.spritesheet('player', 'assets/miner.png', {frameWidth: tileSize, frameHeight: tileSize});
-
+		this.load.spritesheet('player', 'assets/miner.png', {frameWidth: 32, frameHeight: 32});
+		// this.load.spritesheet('ground', 'assets/tiles.png', {frameWidth: 16, frameHeight: 16});
+		this.load.image('ground_top', 'assets/tile.png');
+		this.load.image('ground_plain', 'assets/tile_normal.png');
   }
+	getTilePlayerIsOn() {
+    const playerX = this.player.x + 16;
+    const playerY = this.player.y + 16;
+    const tileX = Math.floor((playerX - this.startX) / tileSize);
+    const tileY = Math.floor((playerY - this.startY) / tileSize) + 1;
+    console.log(tileY);
+		if (tileX >= 0 && tileX < this.groundXLength && tileY >= 0 && tileY < this.groundYLength) {
+        const tileIndex = tileX * this.groundYLength + tileY;
+        return tileIndex;
+    } else {
+        return null;
+    }
+	}
 	create() {
 		// this.physics.world.gravity.y = 300;
 		
     this.player = this.physics.add.sprite((window.innerWidth + tileSize) / 2, 100, 'player');
+		this.player.setSize(18, 32);
+		this.player.setOffset(7, 0);
+		this.player.setScale(1.1);
+		this.ground = this.physics.add.staticGroup();
+		this.groundXLength = Math.floor(worldWidth / tileSize) + 2;
+		this.groundYLength = Math.floor(worldHeight / tileSize) + 2;
+		this.startX = window.innerWidth / 2 - worldWidth / 2;
+		this.startY = 200;
+
+		for(let i = 0; i < this.groundXLength; i++){
+			for(let j = 0; j < this.groundYLength; j++){	
+				let groundTile;
+				if(j == 0){
+					groundTile = this.ground.create(this.startX +  i * (tileSize), this.startY +  j * (tileSize), 'ground_top', 6 * 33 + 6);
+				} else {
+					groundTile = this.ground.create(this.startX +  i * (tileSize), this.startY +  j * (tileSize), 'ground_plain', 6 * 33 + 6);
+				}
+				groundTile.setScale((tileSize) / 32);
+				groundTile.refreshBody();
+			}	
+		}
 		
 		this.physics.add.collider(this.player, this.ground);
 		this.player.setCollideWorldBounds(true);
 		this.player.setBounce(0.2);
 		this.player.setGravityY(300);
-
-
     // Add animations
     this.anims.create({
       key: 'idle',
@@ -68,15 +101,22 @@ class PlanetGeologyScene extends Phaser.Scene {
     // Add keys for input
     this.cursors = this.input.keyboard.createCursorKeys();  // This will give you up, down, left, right keys
   }
+	removeTile(index) {
+    const tile = this.ground.children.entries[index];	
+    tile.setActive(false).setVisible(false).disableBody(true, true); 
+    // tile.destroy(); 
+  }
 
   update(time, delta) {
 		if (this.cursors.right.isDown) {
+			// console.log(this.getTilePlayerIsOn());
       this.player.anims.play('run_right', true);
 			this.player.setVelocityX(playerSpeed);
 			if(this.player.x > window.innerWidth / 2 + worldWidth / 2){
 				this.player.setVelocityX(0);
 			}
     } else if (this.cursors.left.isDown) {
+			// console.log(this.getTilePlayerIsOn());
       this.player.anims.play('run_left', true);
 			this.player.setVelocityX(-playerSpeed);
 			
@@ -85,6 +125,14 @@ class PlanetGeologyScene extends Phaser.Scene {
 			}
     } else if(this.cursors.down.isDown){
 			this.player.anims.play('mine_down', true);
+			// press for at least 100ms
+			if(this.cursors.down.getDuration() > 750){
+				const tileIndex = this.getTilePlayerIsOn();
+				if(tileIndex != null){
+					this.removeTile(tileIndex);
+					this.cursors.down.reset();
+				}
+			}
 		} else {
       this.player.anims.play('idle', true); 
 			this.player.setVelocityX(0);
