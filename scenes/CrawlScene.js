fbea03927374307aa876f846d43458a352613f36
Miroslav Balev <m.balev@editcreation.com>
2025-03-16 10:39:28 +0200

    Add earth background, update final crawl scene and update credits

--- scenes/CrawlScene.js
+++ scenes/CrawlScene.js
@@ -4,12 +4,12 @@ class CrawlScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('backImage', 'assets/planet-background.png');
-    this.load.image('avatar', 'assets/spaceship.png'); 
-    
+    this.load.image('backImage', 'assets/earth/background.png');
+    this.load.image('avatar', 'assets/spaceship.png');
+
     this.load.spritesheet('character', 'assets/AstronautPlayerProfile.png', {
-      frameWidth: 32, 
-  frameHeight: 48   
+      frameWidth: 32,
+      frameHeight: 48
     });
   }
 
@@ -21,24 +21,24 @@ class CrawlScene extends Phaser.Scene {
     backImage.setScale(bgScale).setScrollFactor(0);
 
     this.avatar = this.physics.add.sprite(250, -100, 'avatar').setOrigin(0.5);
-    this.avatar.setScale(2.2); 
+    this.avatar.setScale(2.2);
 
     this.anims.create({
       key: 'walk',
-      frames: this.anims.generateFrameNumbers('character', { start: 0, end: 3 }), 
+      frames: this.anims.generateFrameNumbers('character', { start: 0, end: 3 }),
       frameRate: 8,
       repeat: -1
     });
 
     this.tweens.add({
       targets: this.avatar,
-      y: height / 1.4, 
+      y: height - 240,
       duration: 2000,
       ease: 'Sine.easeInOut',
       onComplete: () => {
-        this.character = this.physics.add.sprite(300, height / 1.2, 'character').setOrigin(0.5);
-    this.character.setScale(2);
-    this.character.play('walk_left') 
+        this.character = this.physics.add.sprite(300, height - 130, 'character').setOrigin(0.5);
+        this.character.setScale(2);
+        this.character.play('walk_left')
         this.startCharacterWalk();
       }
     });
@@ -47,8 +47,8 @@ class CrawlScene extends Phaser.Scene {
   startCharacterWalk() {
     this.tweens.add({
       targets: this.character,
-      x: this.sys.game.canvas.width + 100, 
-      duration: 3500,
+      x: this.sys.game.canvas.width + 100,
+      duration: 5000,
       ease: 'Linear',
       onComplete: () => {
         this.showGameOver();
@@ -59,18 +59,18 @@ class CrawlScene extends Phaser.Scene {
   showGameOver() {
     // Game won text 
     const winText = this.add.text(
-      this.sys.game.canvas.width / 2, 
-      this.sys.game.canvas.height / 2, 
-      'CONGRATILATIONS \nYOU\'VE WON THE GAME!', 
-      { 
-        fontSize: '70px', 
-        fontStyle: 'bold', 
-        color: '#009933', 
-        align: 'center' 
+      this.sys.game.canvas.width / 2,
+      this.sys.game.canvas.height / 2,
+      'CONGRATILATIONS \nYOU\'VE WON THE GAME!',
+      {
+        fontSize: '70px',
+        fontStyle: 'bold',
+        color: '#009933',
+        align: 'center'
       }
     ).setOrigin(0.5)
       .setShadow(5, 5, '#000', 10, true, true);
-    
+
     // Set a 5-second timer to fade out the win text
     this.time.delayedCall(5000, () => {
       // Fade out the win text
@@ -86,9 +86,9 @@ class CrawlScene extends Phaser.Scene {
       });
     });
   }
-  
+
   showCredits() {
-    const creditsText = 
+    const creditsText =
       'CREDITS\n\n' +
       'DEVELOPMENT TEAM\n' +
       'Miroslav Balev\n' +
@@ -99,17 +99,19 @@ class CrawlScene extends Phaser.Scene {
       'ASSETS\n' +
       'Spaceship - https://anim86.itch.io/space-shooter-ship-constructor\n' +
       'Planets & Asteroids - https://deep-fold.itch.io/pixel-planet-generator\n' +
-      'Astronaut - https://floatingkites.itch.io/cute-astronaut\n\n' +
+      'Astronaut - https://floatingkites.itch.io/cute-astronaut\n' +
+      'Planet landscapes - https://www.freepik.com/\n' +
+      'Music - https://archive.org/details/StarWarsThemeSongByJohnWilliams\n\n' +
       'DEVELOPED WITH\n' +
       'Phaser 3\n\n' +
       'THANK YOU FOR PLAYING!';
-    
+
     // Position the credits below the bottom of the screen
     const credits = this.add.text(
-      this.sys.game.canvas.width / 2, 
-      this.sys.game.canvas.height + 500, 
-      creditsText, 
-      { 
+      this.sys.game.canvas.width / 2,
+      this.sys.game.canvas.height + 500,
+      creditsText,
+      {
         fontSize: '36px',
         color: '#ffffff',
         align: 'center',
@@ -117,7 +119,7 @@ class CrawlScene extends Phaser.Scene {
       }
     ).setOrigin(0.5)
       .setShadow(2, 2, '#000', 3, true, true);
-    
+
     // Create a scrolling tween for the credits
     this.tweens.add({
       targets: credits,
